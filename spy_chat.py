af1ff729ba9d229183220b542b76b39a1ef35c9c
ivjotSingh <ivjotofficial@gmail.com>
2017-07-07 21:04:31 +0530

    function for adding friend

--- spy_chat.py
+++ spy_chat.py
@@ -1,7 +1,33 @@
-from spy_details import spy_name,spy_rating,spy_age,spy_salutation,passw,status_messages
+from spy_details import spy_name,spy_rating,spy_age,spy_salutation,passw,status_messages,friends_name,friends_age,friends_is_online,friends_rating
+
+
+#function to add friend
+
+def add_friend():
+    new_name = raw_input("Please add your friend's name: ")
+    new_salutation = raw_input("Are they Mr. or Ms.?: ")
+
+    new_name = new_name + " " + new_salutation
+
+    new_age = raw_input("Age?")
+    new_age = int(new_age)
+
+    new_rating = raw_input("Spy rating?")
+    new_rating = float(new_rating)
+
+    if len(new_name) > 0 and new_age > 12 and new_rating >= spy_rating:
+        friends_name.append(new_name)
+        friends_age.append(new_age)
+        friends_rating.append(new_rating)
+        friends_is_online.append(True)
+        print 'Friend Added!'
+    else:
+        print 'Sorry! Invalid entry. We can\'t add spy with the details you provided'
+
+    return len(friends_name)
+
 
 #function to status update
-
 def add_status(current_status):
 
     if current_status!=None:
@@ -46,7 +72,8 @@ def start_chart(spy_name,spy_age,spy_rating):
 
         #exit the loop here
         elif menu_choice == 2:
-            show_menu = False
+            num=add_friend()
+            print "number of friends are %d"%(num)
 
 
 #asking to continue as a default user
--- spy_details.py
+++ spy_details.py
@@ -5,3 +5,8 @@ spy_rating = 4.7
 passw="bond"
 
 status_messages=['hey','hello']
+
+friends_name = []
+friends_age = []
+friends_rating = []
+friends_is_online = []
